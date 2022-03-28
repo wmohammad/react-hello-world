2939830e852ad7d1987742944e72cea28ea6e383
wmohammad <wraheman7864@gmail.com>
2022-03-28 17:24:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello World</h2>
         <p>
-          Welcome All Buddies to Buddy Webinar!!!
+          Welcome All Friends to Buddy Webinar!!!
         </p>
       </header>
     </div>
